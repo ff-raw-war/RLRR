c3e42897b6c3b40c83f14d9a227774c25afcc918
ff-raw-war <68218644+ff-raw-war@users.noreply.github.com>
2026-07-02 21:49:33 +0800

    Update train.m

--- train.m
+++ train.m
@@ -6,30 +6,30 @@ function [A, Z, E] = train(X, alpha, beta, ep1, ep2, c, epsilon, maxIter)
     errVec2 = [];
     errVec3 = [];
 
-    rho = 1.01; % 原ADMM
-	% rho = 3; % L0LRSSC方法
-    mu1 = 0.1; % 原ADMM和L0LRSSC方法一样，都是0.1
+    rho = 1.01; 
+	% rho = 3; 
+    mu1 = 0.1; 
     mu2 = 0.1;
     mu3 = 0.1;
-    maxMu = 10^5; % L0LRSSC方法是10^6， SSC是10^5
+    maxMu = 10^5; 
     threshold = 2*10^-4; 
     [d, n] = size(X);
     [anchor, ind2, score] = VDA(X', c);
-    % c×d, 选取下标, n×c
-    A = anchor'; % A是d×c
+    
+    A = anchor';
     %{
     scatter(X(:,1), X(:,2), 'b.') % Blue dots
     hold on
     scatter(anchor(:,1), anchor(:,2), 'r+') % Red plus signs
     %}
     Z = rand(c, n);
-    W = ep2./(abs(Z)+ep1); % 根据RSSC来算
+    W = ep2./(abs(Z)+ep1); 
     S = rand(c, n);
     F = rand(n, c);
     E = rand(d, n);
     LZ = rand(n, n);
     % A = rand(d, c);
-    J1 = rand(d, n); % Lagrange乘子
+    J1 = rand(d, n); 
     J2 = rand(n, 1);
     J3 = rand(c, n);
     es1 = ones(c, 1);
@@ -42,7 +42,7 @@ function [A, Z, E] = train(X, alpha, beta, ep1, ep2, c, epsilon, maxIter)
     err3 = 10*threshold;
     errVec3 = [errVec3 err3];
     loss = getLoss(Z, E, W, F, LZ, alpha, beta);
-    lossVec = [lossVec ; loss]; % 记录每一次迭代的loss的值，从第0次开始
+    lossVec = [lossVec ; loss]; 
     iter = 1;
     iterVec = [iterVec iter];
 	notConverged = 1;
@@ -58,20 +58,20 @@ function [A, Z, E] = train(X, alpha, beta, ep1, ep2, c, epsilon, maxIter)
         F = getF(Z);
         H = L2_distance_1(F',F');
         Z = getZ(X, E, A, W, S, H, J1, J2, J3, beta, mu1, mu2, mu3, Z);
-        W = ep2./(abs(Z)+ep1); % 根据RSSC来算
+        W = ep2./(abs(Z)+ep1); 
         A = getA(X, E, Z);
         LZ = getL(Z);
         % tic;
-        % 更改lagrange乘子、学习率μ
+        
         J1 = J1 + mu1*(X-A*Z-E);
         J2 = J2 + mu2*(Z'*es1-es2);
         J3 = J3 + mu3*(W.*Z-S);
-        % mu = ALF; % SSC的mu的算法，是固定值
-        mu1 = min(rho*mu1, maxMu); % GMC-LRSSC方法
+        % mu = ALF;
+        mu1 = min(rho*mu1, maxMu); 
         mu2 = min(rho*mu2, maxMu);
         mu3 = min(rho*mu3, maxMu);
         
-        % 终止迭代条件为约束项
+        
         err1 = norm(X-A*Z,'fro');
         err2 = norm(Z'*es1-es2, 'fro');
         err3 = norm(W.*Z-S, 'fro');
@@ -82,21 +82,21 @@ function [A, Z, E] = train(X, alpha, beta, ep1, ep2, c, epsilon, maxIter)
         errVec2 = [errVec2 err2];
         errVec3 = [errVec3 err3];
         lossVec = [lossVec loss];
-        iterVec = [iterVec iter]; % 记录迭代次数，从第0此开始
-		% 检查是否收敛
+        iterVec = [iterVec iter];
+		
         
 		if iter >= maxIter
 			notConverged = 0;
         end
         
         %{
-        % 约束项收敛
+       
 		if err1 < threshold && err2<threshold && err3<threshold
 			notConverged = 0;
 		end
         %}
 		
-        % 终止迭代条件为loss值差
+        
         if ((loss0-loss)'*(loss0-loss) < epsilon) || (loss > loss0)
             notConverged = 0;
         end % 
@@ -106,7 +106,7 @@ function [A, Z, E] = train(X, alpha, beta, ep1, ep2, c, epsilon, maxIter)
     end % while iter
     
     
-    % 画图用
+    
     %plot(hengzhou, zongzhou, 'd-');
     if iter < 10
         for i = 1:10-iter
